bc2bf165a310f1ef054fb5114cf1f4cff973ee74
razibh <razib11@yahoo.com>
2022-12-05 19:39:53 +0600

    final

--- block.js
+++ block.js
@@ -1,6 +1,6 @@
 const sha256 = require("crypto-js/sha256")
 class Block{
-    constructor(timestamp,data, previoushash){
+    constructor(timestamp,data, previoushash = ""){
         this.timestamp = timestamp;
 
         this.data = data;
@@ -15,14 +15,45 @@ class Block{
 
 class Blockchain {
     constructor (){
-        this.chain = [];
-}
-    addBlock(newBlock){
-    this.chain.push(newBlock);
-}
+        this.chain = [this.generateGenesisBlocl()];
 
 }
+generateGenesisBlocl(){
+    return new Block("2022-12-05", "GENESIS", "0000 ");
+}
+    getLatestBlock(){
+        return this.chain[this.chain.length -1];
+    }
+    addBlock(newBlock){
+        newBlock.previoushash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    
+}
+    isBlockchainValid(){
+        for (let i=1; i<this.chain.length; i++)
+        {
+            const  CurrantBlock = this.chain[i];
+            const previoushBlock = this.chain[i-1];
+            if ( CurrantBlock.hash !== CurrantBlock.calculateHash())
+            {
+                return false;
+            }
+            if (CurrantBlock.previoushash !== previoushBlock.hash)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    }
+
 const josscoin = new Blockchain();
-const block =new Block("2019-01-01", { amount: 5 }, "ABCD");
+const block =new Block("2022-12-04", { amount: 5 }, "ABCD");
 josscoin.addBlock(block);
-console.log(josscoin); 
+console.log(josscoin.isBlockchainValid());
+ josscoin.chain[1].data = "Haked";
+ console.log(josscoin.isBlockchainValid());
+ 
+
+ 
